bc7cbd8ba7a13580479e2f7ab6b0cc17b433d339
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2024-02-12 20:58:12 +0900

    update: 画像処理をsharpに変更

--- lib/createXProps.ts
+++ lib/createXProps.ts
@@ -1,5 +1,6 @@
 import { FeedEntry } from 'https://deno.land/x/rss@0.6.0/src/types/mod.ts';
 
+// deno-lint-ignore require-await
 export default async ({ item }: { item: FeedEntry }) => {
   const title: string = (item.title?.value || '').trim();
   const link: string = item.links[0].href || '';
